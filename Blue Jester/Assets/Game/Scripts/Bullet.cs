564e79bc4aef6e2384b40f907258769454e22e1d
Leonardo Sasseron <leo.dias360@gmail.com>
2019-03-18 11:48:55 -0300

    First Commit
    - Move
    - Crouch
    - Roll
    - Shoot
    -Enemy

---
+++ Blue Jester/Assets/Game/Scripts/Bullet.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Bullet : MonoBehaviour 
+{
+    public float speed = 20f;
+    public int damage = 40;
+    public Rigidbody2D rb;
+    public GameObject impactEffect;    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PlatformerCharacter2D.m_FacingRight)
+        {
+            rb.velocity = transform.right * speed;
+        }
+
+        else
+        {
+            rb.velocity = transform.right * -1 * speed;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }   
+}
---
+++ Blue Jester/Assets/Game/Scripts/Roll.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Roll : MonoBehaviour
+{
+    Animator animator;
+    public Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool r = Input.GetKeyDown(KeyCode.W); 
+
+        animator.SetBool("Roll", r);
+    }
+}
